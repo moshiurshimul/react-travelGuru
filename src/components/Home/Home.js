6778c1d91f6bff2f279f60332b10130e949716dd
moshiurshimul <hello@moshiurshimul.com>
2020-10-13 01:00:34 +0600

    style added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,8 @@ import './Home.css';
 import locationimage1 from '../../Image/Sajek.png';
 import locationimage2 from '../../Image/Sreemongol.png';
 import locationimage3 from '../../Image/sundorbon.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight} from '@fortawesome/free-solid-svg-icons';
 
 
 const Home = () => {
@@ -19,6 +21,7 @@ const Home = () => {
                         <Card.Text>
                             Sajek is a union located in the north of Chittagong Hill Tracts. It's under Baghaichori Upazila in Rangamati hill district.
                         </Card.Text>
+                        <button className="book-now-btn">Book Now <FontAwesomeIcon icon={faArrowRight} /></button>
                     </Card.ImgOverlay>
                 </Card>
                 <Card className="text-white">
@@ -28,6 +31,7 @@ const Home = () => {
                         <Card.Text>
                             It is said the name Sreemangal (or Srimangal) is named after Sri Das and Mangal Das; two brothers who settled on the banks of the Hail Haor.
                         </Card.Text>
+                        <button className="book-now-btn">Book Now <FontAwesomeIcon icon={faArrowRight} /></button>
                     </Card.ImgOverlay>
                 </Card>
                 <Card className="text-white">
@@ -37,6 +41,7 @@ const Home = () => {
                         <Card.Text>
                             The Sundarbans is a mangrove area in the delta formed by the confluence of the Ganges, Brahmaputra and Meghna Rivers in the Bay of Bengal.
                         </Card.Text>
+                        <button className="book-now-btn">Book Now <FontAwesomeIcon icon={faArrowRight} /> </button>
                     </Card.ImgOverlay>
                 </Card>
             </CardDeck>
