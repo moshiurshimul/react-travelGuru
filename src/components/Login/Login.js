672d12c7e1cd34b8a63f878ffb0bc6ecc886865a
moshiurshimul <hello@moshiurshimul.com>
2020-10-16 01:59:26 +0600

    conditional signin

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
@@ -12,35 +12,96 @@ import fbicon from '../../Icon/fb.png';
 firebase.initializeApp(firebaseConfig);
 
 const Login = () => {
+    // User State
+    const [user, setUser] = useState({
+        isSignIn: false,
+        newUser: false,
+        name: '',
+        email:'',
+        password:''
+    });
+
+    // Login using Google
     const googleProvider = new firebase.auth.GoogleAuthProvider();
 
     const handleGoogleSignIn = () => {
         firebase.auth().signInWithPopup(googleProvider)
         .then(result => {
-            const user = result.user;
-            console.log(user);
+            const {displayName, email} = result.user;
+            const signInUser = {
+                isSignIn: true,
+                name: displayName,
+                email: email
+            }
+            setUser(signInUser);
           })
           .catch(function(error) {
             // Handle Errors here.
             // ...
           });
     } 
+
+    // Login Using Email & Password form 
+
+    const handleBlur = (e) => {
+        let isFieldmValid = true;
+        if(e.target.name === 'email'){
+            isFieldmValid = /\S+@\S+\.\S+/.test(e.target.value);
+        }
+        if(e.target.name === 'password') {
+            if(e.target.value.length < 6) {
+                alert('Password must be more than 6 characters')
+            }
+
+            isFieldmValid = e.target.value.length > 6;
+            
+        }
+        if(isFieldmValid) {
+            const newUserInfo = {...user};
+            newUserInfo[e.target.name] = e.target.value;
+            setUser(newUserInfo);
+        }
+    }
+
+    const handleFormSubmit = (e) => {
+        if(user.email && user.password) {
+            firebase.auth()
+            .createUserWithEmailAndPassword(user.email, user.password) 
+            .catch(error => {
+                // Handle Errors here.
+                var errorMessage = error.message;
+                alert(error.message);
+              });
+        }
+        e.preventDefault();
+    }
+
+    const handleCreateAccount = () => {
+        const userInfo = {...user};
+        userInfo.newUser = true;
+        setUser(userInfo);
+        console.log(user)
+    }
     return (
         <div className="login-page">
             <div className="login-container">
                 <div className="login-form">
-                    <Form>
-                        <h4>Login</h4>
+                    <Form onSubmit={handleFormSubmit}>
+                        <h4>{(user.newUser === true) ? 'Create an account' : 'Login'} </h4>
                         <Form.Group controlId="formBasicEmail">
-                            <Form.Control type="email" placeholder="Enter Email" required/>
+                            <Form.Control onBlur={handleBlur} type="text" name="name" placeholder="Your Name" required/>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Control onBlur={handleBlur} type="email" name="email" placeholder="Enter Email" required/>
                         </Form.Group>
                         <Form.Group controlId="formBasicPassword">
-                            <Form.Control type="password" placeholder="Password" required/>
+                            <Form.Control onBlur={handleBlur} type="password" name="password" placeholder="Password" required/>
                         </Form.Group>
                         <Button variant="warning btn-block" type="submit">
                             Login
                         </Button>
                     </Form>
+                    <p style={{textAlign:"center", marginTop:"20px"}}>Don't have a account? <button className="text-warning create-account-btn" onClick={handleCreateAccount}>Create an account</button> </p>
                 </div>
                 <hr/>
                 <div className="login-btn">
